069fac0dc5c7cc40a1d769e5a3c87d2e6d64f10c
Svetla <svetla.syrimis@gmail.com>
2019-11-14 14:44:00 -0500

    changes

--- models.js
+++ models.js
@@ -9,56 +9,56 @@ if (process.env.DATABASE_URL) {
   });
 } else {
   sequelize = new Sequelize({
-  database: 'games_db',
-  dialect: 'postgres',
-  define: {
-    underscored: true
-  }
-});
-
-class User extends Sequelize.Model { };
-
-User.init({
-  username: Sequelize.STRING,
-  password_digest: Sequelize.STRING
-}, {
-  sequelize,
-  modelName: 'user'
-});
-
-class Game extends Sequelize.Model { };
-
-Game.init({
-  name: Sequelize.STRING,
-  image_url: Sequelize.TEXT,
-  description: Sequelize.TEXT,
-}, {
-  sequelize,
-  modelName: 'game'
-});
-
-class Review extends Sequelize.Model { };
-
-Review.init({
-  review: Sequelize.TEXT(500)
-}, {
-  sequelize,
-  modelName: 'review'
-});
-
-User.hasMany(Game, { onDelete: 'cascade' });
-Game.belongsTo(User);
-
-User.hasMany(Review, { onDelete: 'cascade' })
-Review.belongsTo(User);
-
-Game.hasMany(Review, { onDelete: 'cascade' })
-Review.belongsTo(Game);
-
-module.exports = {
-  Game,
-  User,
-  Review,
-  sequelize
+    database: 'games_db',
+    dialect: 'postgres',
+    define: {
+      underscored: true
+    }
+  });
 }
+  class User extends Sequelize.Model { };
+
+  User.init({
+    username: Sequelize.STRING,
+    password_digest: Sequelize.STRING
+  }, {
+    sequelize,
+    modelName: 'user'
+  });
+
+  class Game extends Sequelize.Model { };
+
+  Game.init({
+    name: Sequelize.STRING,
+    image_url: Sequelize.TEXT,
+    description: Sequelize.TEXT,
+  }, {
+    sequelize,
+    modelName: 'game'
+  });
+
+  class Review extends Sequelize.Model { };
+
+  Review.init({
+    review: Sequelize.TEXT(500)
+  }, {
+    sequelize,
+    modelName: 'review'
+  });
+
+  User.hasMany(Game, { onDelete: 'cascade' });
+  Game.belongsTo(User);
+
+  User.hasMany(Review, { onDelete: 'cascade' })
+  Review.belongsTo(User);
+
+  Game.hasMany(Review, { onDelete: 'cascade' })
+  Review.belongsTo(Game);
+
+  module.exports = {
+    Game,
+    User,
+    Review,
+    sequelize
+  }
 
